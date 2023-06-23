e93cbcd8674f038eec5c7b737aac1586196ebb32
Brandon <branyzp@gmail.com>
2023-06-24 01:01:29 +0800

    remove material UI theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DarkModeContext } from '../hooks/DarkModeContext';
 import LightSwitch from './LightSwitch';
-import { AppBar } from '@mui/material';
+import Logo from './Logo';
+import NavbarItem from './NavbarItem';
+const navBarArr = ['resources', 'register', 'login'];
+const authenticatedNavBarArr = ['dashboard', 'expenses', 'user', 'logout'];
 
-const navBarArr = ['resources', 'login', 'register'];
-const authNavBarArr = ['dashboard', 'user', 'logout'];
+function Navbar({ isAuthenticated }) {
+	const { darkmode } = useContext(DarkModeContext);
 
-function Navbar() {
 	return (
-		<AppBar color="primary">
-			<div className="flex flex-row justify-between">
-				<h1 className="">Hi</h1>
-				<h1 className="">Hi</h1>
-				<LightSwitch />
+		<div
+			className={
+				darkmode
+					? 'bg-slate-900 bg-gradient-to-tl from-slate-900 text-slate-200 bg-opacity-50 fixed w-full h-20 top-0 shadow-xl z-[100] backdrop-blur-sm '
+					: 'bg-slate-100 bg-opacity-50 fixed w-full h-20 top-0 shadow-xl z-[100] backdrop-blur-sm text-slate-800'
+			}
+		>
+			<div className="flex justify-between items-center w-full h-full px-2">
+				<Logo />
+
+				<div>
+					{isAuthenticated ? (
+						<ul className="flex px-5">
+							{authenticatedNavBarArr.map((navitem, index) => {
+								return <NavbarItem key={index} navitem={navitem} />;
+							})}
+							<li className="ml-10 py-3">
+								<LightSwitch />
+							</li>
+						</ul>
+					) : (
+						<ul className="flex px-5">
+							{navBarArr.map((navitem, index) => {
+								return <NavbarItem key={index} navitem={navitem} />;
+							})}
+							<li className="ml-10 py-3">
+								<LightSwitch />
+							</li>
+						</ul>
+					)}
+				</div>
 			</div>
-		</AppBar>
+		</div>
 	);
 }
 
